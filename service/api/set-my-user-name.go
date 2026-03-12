f34aab617650e7884d0cfdd84c9605111db0259f
aaitayev <aitayev.2106823@studenti.uniroma1.it>
2026-03-12 16:33:27 +0100

    Update set-my-user-name.go

--- service/api/set-my-user-name.go
+++ service/api/set-my-user-name.go
@@ -4,7 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"strings"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/api/reqcontext"
+	"https://github.com/aaitayev/wasa-homework.git"
 	"github.com/julienschmidt/httprouter"
 )
 
