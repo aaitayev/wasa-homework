197aa7c98dae1923f3ff6d50db11d2384fc28925
aaitayev <aitayev.2106823@studenti.uniroma1.it>
2026-03-19 16:19:38 +0100

    Update send-message.go

--- service/api/send-message.go
+++ service/api/send-message.go
@@ -6,8 +6,8 @@ import (
 	"strings"
 	"time"
 
-	"https://github.com/aaitayev/wasa-homework.git"
-	"https://github.com/aaitayev/wasa-homework.git"
+	"github.com/aaitayev/wasa-homework.git"
+	"github.com/aaitayev/wasa-homework.git"
 	"github.com/gofrs/uuid"
 	"github.com/julienschmidt/httprouter"
 )
