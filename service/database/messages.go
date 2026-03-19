552d3578253d24727dab378545beae13f518dad5
aaitayev <aitayev.2106823@studenti.uniroma1.it>
2026-03-19 16:21:02 +0100

    Update messages.go

--- service/database/messages.go
+++ service/database/messages.go
@@ -3,7 +3,7 @@ package database
 import (
 	"database/sql"
 	"errors"
-	"git.sapienzaapps.it/fantasticcoffee/fantastic-coffee-decaffeinated/service/models"
+	"github.com/aaitayev/wasa-homework.git"
 	"time"
 )
 
